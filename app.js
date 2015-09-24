5ef487752eb5753f91942ea309d4bde7aa9b7ddd
Gregory Zoeller <gregory.zoeller@homesite.com>
2015-09-24 08:04:30 -0400

    modifying port listener

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', './views');
@@ -42,9 +46,6 @@ app.post('/therapist', function(req, res){
 	res.sendStatus(200);
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
